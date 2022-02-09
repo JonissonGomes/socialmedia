0da290996a906b9712cfe2a94d47982485f20589
JonissonGomes <joni_gomes@hotmail.com>
2022-02-09 20:16:32 -0300

    Inicializando e criando header da aplicação

--- media-academy/src/pages/home.tsx
+++ media-academy/src/pages/home.tsx
@@ -1,11 +1,55 @@
-import * as React from 'react';
-type Props = {
-    
+import { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import * as React from "react";
+
+import logoGlobo from "../utils/images/logo-academy.png";
+import styles from "../styles/homepage.module.css";
+
+const Home: NextPage = () => {
+  const project = "</ Academy>";
+
+  return (
+    <div className={styles.header}>
+      <section className={styles.boxUrl}>
+        <Link href={"/"}>
+          <p className={styles.colorBlue}>globo.com</p>
+        </Link>
+        <p className={styles.divisor}>|</p>
+        <Link href={"/"}>
+          <p className={styles.colorGreen}>g1</p>
+        </Link>
+        <p className={styles.divisor}>|</p>
+        <Link href={"/"}>
+          <p className={styles.colorOrange}>ge</p>
+        </Link>
+        <p className={styles.divisor}>|</p>
+        <Link href={"/"}>
+          <p className={styles.colorBlue}>vídeos</p>
+        </Link>
+        <p className={styles.divisor}>|</p>
+        <Link href={"/"}>
+          <p className={styles.colorDarkBlue}>o globo</p>
+        </Link>
+      </section>
+
+      <section className={styles.boxRight}>
+        <Link href={"/home"}>
+          <p className={styles.options}>ASSINE JÁ</p>
+        </Link>
+
+        <Link href={"/home"}>
+          <p className={styles.options}>E-MAIL</p>
+        </Link>
+
+        <Link href={"/home"}>
+          <button className={styles.optionsButton}>
+            <p className={styles.options}>{"ENTRAR >"}</p>
+          </button>
+        </Link>
+      </section>
+    </div>
+  );
 };
-export const home = (props: Props) => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+
+export default Home;
