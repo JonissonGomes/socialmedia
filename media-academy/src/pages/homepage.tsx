88de017e1736c8864c0a37318434e3d12530f917
JonissonGomes <joni_gomes@hotmail.com>
2022-02-10 16:18:29 -0300

    Adicionando dinamicidade a div do Noticias rápidas

--- media-academy/src/pages/homepage.tsx
+++ media-academy/src/pages/homepage.tsx
@@ -183,7 +183,6 @@ const HomePage: NextPage = () => {
           <div className={styles.newsFasterBox}>
             <p className={styles.titleFastHeader}>Notícias rápidas</p>
               {fastNews.slice(0,5).map((newsFast, index) => (
-                <>
                   <div key={index} className={styles.newsFaster}>
                     <section className={styles.newsContent}>
                       <p className={styles.titleFastNews}>{newsFast.title}</p>
@@ -199,7 +198,6 @@ const HomePage: NextPage = () => {
                       </Link>
                     </section>
                   </div>
-                </>
               ))}
           </div>
         </div>
