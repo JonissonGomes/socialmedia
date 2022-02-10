8af11d0be67da04a2087548ccec293f1d959ebe8
JonissonGomes <joni_gomes@hotmail.com>
2022-02-10 12:04:50 -0300

    Adicionando rota de dominio do buscador de imagens

--- media-academy/next.config.js
+++ media-academy/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["ogimg.infoglobo.com.br"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
