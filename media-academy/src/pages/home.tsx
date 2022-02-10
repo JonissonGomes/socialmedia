abc4c6426e0b5db78a51c41c8fc289a35ccfe3c4
JonissonGomes <joni_gomes@hotmail.com>
2022-02-09 21:16:09 -0300

    Criando conteúdo de notícias

--- media-academy/src/pages/home.tsx
+++ media-academy/src/pages/home.tsx
@@ -7,7 +7,35 @@ import logoGlobo from "../utils/images/logo-academy.png";
 import styles from "../styles/homepage.module.css";
 
 const Home: NextPage = () => {
-  const project = "</ Academy>";
+
+    const noticias = [
+        {
+            title: "Dev da delete sem where e acaba causando um acidente",
+            describe: "O jovem possuia apenas 22 anos e hoje está procurando onde esconder a cabeça",
+            date: '09/02/2022'
+        },
+        {
+            title: "Região metropolitana recebe novos eventos para desmistificar o flutter",
+            describe: "Aparentemente o novo surto do novo framework está causando complicações",
+            date: '09/02/2022'
+        },
+        {
+            title: "Após estourar tempo limite do projeto, estudante acaba indo tomar energético para melhorar sua performance",
+            describe: "O adolecente procurou utilizar os mais diversos métodos quando acidentalmente esqueceu de enviar seu projeto no gmail",
+            date: '09/02/2022'
+        },
+        {
+            title: "Região metropolitana recebe novos eventos para desmistificar o flutter",
+            describe: "Aparentemente o novo surto do novo framework está causando complicações",
+            date: '09/02/2022'
+        },
+        {
+            title: "Após estourar tempo limite do projeto, estudante acaba indo tomar energético para melhorar sua performance",
+            describe: "O adolecente procurou utilizar os mais diversos métodos quando acidentalmente esqueceu de enviar seu projeto no gmail",
+            date: '09/02/2022'
+        },
+    ]
+
 
   return (
     <div>
@@ -52,18 +80,35 @@ const Home: NextPage = () => {
       </div>
 
       <div className={styles.navBar}>
-          <section className={styles.logo}>
-            <Image  src={logoGlobo} height={46} width={46} />
-          </section>
+        <section className={styles.logo}>
+          <Image src={logoGlobo} height={46} width={46} />
+        </section>
 
-          <section className={styles.logo}>
-            <p className={styles.fakeG1}>G1</p>
-          </section>
+        <section className={styles.logo}>
+          <p className={styles.fakeG1}>G1</p>
+        </section>
 
-          <section className={styles.logo}>
-            <input className={styles.input} type="text" placeholder="Buscar" />
-          </section>
+        <section className={styles.logo}>
+          <input className={styles.input} type="text" placeholder="Buscar" />
+        </section>
+      </div>
 
+      <div className={styles.contentMain}>
+          <h1 className={styles.topic}>Notícias de dev</h1>
+
+          <hr className={styles.separator} />
+        {
+              noticias.map( 
+                  (noticias) => 
+                      <>
+                        <div className={styles.news}>
+                            <p className={styles.titleNews}>{noticias.title}</p>
+                            <p className={styles.describeNews}>{noticias.describe}</p>
+                            <p className={styles.dateNews}>{noticias.date}</p>
+                        </div>
+                    </>
+                )
+            }
       </div>
     </div>
   );
